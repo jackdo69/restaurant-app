7b3942e65b1d4226261ee6ebb53aed2c10cdda76
jackdo69 <jackie.duc.do@gmail.com>
2020-01-11 01:31:11 +1100

    add a auth page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
+import classes from './Auth.module.css';
+import Input from '../../components/Input/Input';
 
 class Auth extends Component {
-    state = {  }
+    state = { 
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Mail address'
+                },
+                value: ''
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: ''
+            }
+        }
+     }
     render() { 
+        const formElementsArray = [];
+        for (let key in this.state.controls) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.controls[key]
+            })
+        }
+
+        let form = formElementsArray.map(formElement => (
+            <Input
+                key={formElement.id}
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}
+                changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+        ))
         return ( 
-            <h2>Auth Component</h2>
+            <div className={classes.Auth}>
+                <form onSubmit={this.submitHandler}>
+                    {form}
+                    <button>SUBMIT</button>
+                </form>
+                <button
+                clicked={this.switchAuthModeHandler}>SWITCH MODE</button>
+            </div>
          );
     }
 }
