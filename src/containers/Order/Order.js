7d78252fc643d687644b2057d303dab5bcc882da
jackdo69 <jackie.duc.do@gmail.com>
2020-01-16 23:22:02 +1100

    complete order function

--- src/containers/Order/Order.js
+++ src/containers/Order/Order.js
@@ -7,12 +7,16 @@ import { Redirect } from 'react-router-dom';
 import * as actions from '../../store/actions/index';
 import classes from './Order.module.css';
 import Button from '../../components/Button/Button';
+import Modal from '../../components/Modal/Modal';
+import OrderConfirm from './OrderConfirm/OrderConfirm'
 
 
 class Order extends Component {
+    state = {
+        showModal: false
+    }
 
-    orderHandler = (event) => {
-        event.preventDefault();
+    orderHandler = () => {
         const order = {
             foods: this.props.foods,
             userId: this.props.userId
@@ -22,28 +26,46 @@ class Order extends Component {
         this.props.clearOrder();
     }
 
-    // componentDidUpdate() {
-    //     this.props.clearOrder();
-    // }
+    onConfirm = () => {
+        this.setState({ showModal: true })
+    }
+
+    purchaseCancel = () => {
+        this.setState({ showModal: false })
+    }
 
     render() {
-        
+        let orderConfirm = null;
+        if(this.props.foods.length) {
+            orderConfirm = (
+                <Modal 
+                show={this.state.showModal}
+                modalClosed={this.purchaseCancel}>
+                    <OrderConfirm 
+                        foods={this.props.foods}
+                        cancel={this.purchaseCancel}
+                        proceed={ this.orderHandler}/>
+                </Modal>
+            )
+        }
+
         let authRedirect = null;
         if (!localStorage.getItem('token')) {
             authRedirect = <Redirect to={this.props.authRedirectPath} />
         }
 
         return (
-            <Aux>         
+            <Aux>
                 <div className={classes.Order}>
                     <h2 align="center">Order Takeaway</h2>
                     {authRedirect}
+                    {orderConfirm}
                     <OrderSummary />
                     <OrderControls />
                     <Button
                         disabled={!this.props.foods.length}
                         btnType='Success'
-                        click={this.orderHandler}>PLACE ORDER</Button>
+                        click={this.onConfirm}>PLACE ORDER</Button>
                 </div>
 
             </Aux>
---
+++ src/containers/Order/OrderConfirm/OrderConfirm.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import classes from './OrderConfirm.module.css';
+import Button from '../../../components/Button/Button';
+
+class OrderConfirm extends Component {
+
+    render() {
+        let totalPrice = 0;
+        return (
+            <div className={classes.OrderConfirm}>
+                <h3>Your Order</h3>
+                {this.props.foods.map(food => {
+                    totalPrice += food.price
+                    return (
+                        <div className={classes.row3}>
+                            <div>{food.name}</div>
+                            <div></div>
+                            <div>${food.price}</div>
+                        </div>
+                    )
+                })}
+                <hr />
+                <div className={classes.row3}>
+                    <div><h4>Total</h4></div>
+                    <div></div>
+                    <div> ${totalPrice.toFixed(2)}</div>
+                </div>
+                <Button click={this.props.cancel} btnType='Danger'>Cancel</Button>
+                <Button click={this.props.proceed} btnType='Success'>Proceed</Button>
+                
+            </div>
+        );
+    }
+}
+
+export default OrderConfirm;
