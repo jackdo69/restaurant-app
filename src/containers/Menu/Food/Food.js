13c9192d2180b1cd48660395aa7c324302ab119b
Jack Do <jackie.duc.do@gmail.com>
2021-11-02 17:51:05 +0700

    more on responsive

--- src/containers/Menu/Food/Food.js
+++ src/containers/Menu/Food/Food.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import classes from './Food.module.css';
 
-
 class Food extends Component {
-    render() {
-        return (
-            <div 
-                className={classes.Food}
-                onClick={this.props.clicked}>               
-                <img src={this.props.imgURL} alt='' />
-                <h4>{this.props.id}. {this.props.name}</h4>
-                <p> {this.props.description.substring(0, 55)}....</p>
-                <div className={classes.Price}>$ {this.props.price}</div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={classes.Food} onClick={this.props.clicked}>
+        <div className={classes.ImageContainer}>
+          <img src={this.props.imgURL} alt='' />
+        </div>
+        <h4>
+          {this.props.id}. {this.props.name}
+        </h4>
+        <p> {this.props.description.substring(0, 55)}....</p>
+        <div className={classes.Price}>$ {this.props.price}</div>
+      </div>
+    );
+  }
 }
 
 export default Food;
